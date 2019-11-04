cafb7bf07401f83bee4b88c32ec120a3fe47a16d
Cragzu <cragzu@gmail.com>
2019-11-04 14:24:49 -0800

    Created input loop function

--- lab08/test_input_loop.py
+++ lab08/test_input_loop.py
@@ -1,7 +1,19 @@
 from unittest import TestCase
 from maze import input_loop
+import io
+import unittest.mock
 
 
 class TestInputLoop(TestCase):
-    def test_input_loop(self):
-        self.fail()
+
+    @unittest.mock.patch('builtins.input', return_value='n')
+    def test_return_value(self, mock_input):  # check that returned string is the user choice
+        self.assertEqual(input_loop('Test', ['N']), 'N')
+
+    @unittest.mock.patch('builtins.input', side_effect=['a', 'b', 'n'])
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_invalid_input(self, mock_stdout, mock_input):  # check for error messages on invalid input
+        expected_output = 'Sorry, that wasn\'t a valid input. Please try again.\n' \
+                          'Sorry, that wasn\'t a valid input. Please try again.\n'
+        input_loop('Test', ['N'])
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
