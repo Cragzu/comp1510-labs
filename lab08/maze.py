da632ec1bdc67c143f978058b1977b0938196075
Cragzu <cragzu@gmail.com>
2019-11-04 13:43:51 -0800

    Initial commit

---
+++ lab08/maze.py
@@ -0,0 +1,20 @@
+"""
+A module to simulate a small version of a Single User Dungeon.
+"""
+
+
+def game():
+    board = [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]]  # 5x5 matrix
+    character = [0, 0]  # starting coordinates
+
+
+
+def main():
+    """
+    Drive the program.
+    """
+    game()
+
+
+if __name__ == "__main__":
+    main()
