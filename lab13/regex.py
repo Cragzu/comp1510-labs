87bbda48a9ba2bbf3e01fdf73aba0ce8450cf0b7
Chloe (Desktop) <cragzu@gmail.com>
2019-12-06 19:04:04 -0800

    Created Nakamoto name validation

--- lab13/regex.py
+++ lab13/regex.py
@@ -27,14 +27,41 @@ def validate_email(string: str) -> str:
     else:
         return 'No email found.'
 
+
+def nakamoto(name: str) -> str:
+    """
+    Validate a name with the surname Nakamoto.
+
+    For the name to be valid:
+        - the first name must be only letters, starting with a capital letter
+        - there should be a space between first and last names
+        - the last name should be Nakamoto
+
+    :param name: str, the name to be validated
+    :return: str, a message saying whether or not the given name is valid
+    """
+    match = re.compile(r'[A-Z][a-z]+\s(Nakamoto)')
+
+    match_object = match.search(name)
+    if match_object:
+        return 'You are a valid member of the Nakamoto family, ' + match_object.group()
+    else:
+        return 'That is no family member of ours!'
+
+
+
 def main():
     """
     Drive the program.
     """
 
     # Email validation
-    print(validate_email('My email is apple@abc.com'))
-    print(validate_email('My email is aaaaaaaaa'))
+    # print(validate_email('My email is apple@abc.com'))
+    # print(validate_email('My email is aaaaaaaaa'))
+
+    # Nakamoto name validation
+    print(nakamoto('My name is Angel Nakamoto'))
+    print(nakamoto('ann Nakamoto'))
 
 
 if __name__ == "__main__":
